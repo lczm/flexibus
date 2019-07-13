cd58809ee3be34bc18a5a412f693beaa66a1ed06
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2019-07-13 09:47:51 +0800

    Added routes in data.py

--- backend/data.py
+++ backend/data.py
@@ -1,4 +1,6 @@
 import pickle
+import random
+import numpy as np
 
 class Data:
     def __init__(self):
@@ -23,4 +25,46 @@ class Data:
             'Latitude': latitudes,
             'Longitude': longitudes,
             'Weight': weights
+        }
+
+    def routes(self, n_routes, n_stops):
+        routes = []
+        for _ in range(n_routes):
+            code = random.choice(list(self.data.keys()))
+            stop = self.data[code]
+            route = [code]
+            for _ in range(n_stops):
+                links_taps = [sum(self.data[code2]['Hourly Taps'].values()) for code2 in stop['Links']]
+                indicies = np.argsort(links_taps)
+                
+                success = False
+                for index in indicies[::-1]:
+                    code = stop['Links'][index]
+                    if code not in route:
+                        route.append(code)
+                        stop = self.data[code]
+                        success = True
+                        break
+                
+                if not success:
+                    break
+            
+            routes.append(route)
+
+
+        latitudes = []
+        longitudes = []
+        for route in routes:
+            latitude = []
+            longitude = []
+            for code in route:
+                stop = self.data[code]
+                latitude.append(stop['Latitude'])
+                longitude.append(stop['Longitude'])
+            latitudes.append(latitude)
+            longitudes.append(longitude)
+
+        return {
+            'Longitude': longitudes,
+            'Latitude': latitudes
         }
